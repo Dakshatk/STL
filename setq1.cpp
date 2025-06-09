1f3e7623cd480facc71f61f8aa01312ce101df2d
Dakshatk <dakshat25k@gmail.com>
2025-06-09 19:34:47 +0530

    first commit

---
+++ setq1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<set>
+using namespace std;
+int main(){
+    int i,a[6]={};
+        for(int i=0;i<6;i++){
+            cin>>a[i];
+        }
+        set<int>b;
+        for(int i=0;i<6;i++){
+            b.insert(a[i]);
+            cout<<a[i]<<" ";
+        }
+        cout<<endl;
+            cout<<b.size();
+    return 0;
+}
